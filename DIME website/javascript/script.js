c231d5f20f9474b1cbff71f01a9bbcec6c357f6a
jamesfallon99 <53617404+jamesfallon99@users.noreply.github.com>
2019-12-10 21:40:35 +0000

    Add files via upload

---
+++ DIME website/javascript/script.js
@@ -0,0 +1,44 @@
+
+//For Home
+$(document).ready(function() {
+   $("#Home").hover(function() {
+      $(this).css("background", "#D86826");
+   },
+   function() {
+      $(this).css("background", "#FE8F10");
+
+   });
+});
+
+//For uncover your passion
+$(document).ready(function() {
+   $("#Uncover_your_passion").hover(function() {
+      $(this).css("background", "#D86826");
+   },
+   function() {
+      $(this).css("background", "#FE8F10");
+
+   });
+});
+
+//Step by step
+$(document).ready(function() {
+   $("#Step_by_step").hover(function() {
+      $(this).css("background", "#D86826");
+   },
+   function() {
+      $(this).css("background", "#FE8F10");
+
+   });
+});
+
+//Where you go matters
+$(document).ready(function() {
+   $("#Where_you_go_matters").hover(function() {
+      $(this).css("background", "#D86826");
+   },
+   function() {
+      $(this).css("background", "#FE8F10");
+
+   });
+});
